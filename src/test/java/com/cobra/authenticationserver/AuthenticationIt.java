81d1149405ab14ccbabbf6326a73b4cd6ad2d657
Octothorp6 <blora616@gmail.com>
2020-03-23 17:51:12 -0400

    auth integration testing

--- src/test/java/com/cobra/authenticationserver/AuthenticationIt.java
+++ src/test/java/com/cobra/authenticationserver/AuthenticationIt.java
@@ -15,9 +15,11 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -38,16 +40,79 @@ public class AuthenticationIt {
 
     @Test
     public void testGetToken() throws Exception {
-        MvcResult result = this.mockMvc.perform(post("/oauth/token")
-                .with(httpBasic(clientId,clientSecret))
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content("grant_type=password&username=tester&password=123456"))
-                .andExpect(status().isOk())
-                .andReturn();
+        // Invoke the token url and receive a token in return
+        MvcResult result = getResult();
 
         String tokenString = result.getResponse().getContentAsString();
 
         Map<String, Object> map = mapper.readValue(tokenString, new TypeReference<Map<String, Object>>() {});
         assertTrue(map.containsKey("access_token"));
+    }
+
+    @Test
+    public void testCheckToken() throws Exception {
+        // Invoke the token url and receive a token in return
+        MvcResult result = getResult();
+
+        String tokenString = result.getResponse().getContentAsString();
+        // Map the response
+        Map<String, Object> map = mapper.readValue(tokenString, new TypeReference<Map<String, Object>>() {});
+
+        result = this.mockMvc.perform(post("/oauth/check_token")
+                .with(httpBasic(clientId,clientSecret))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .content("token=" +map.get("access_token")))
+                .andExpect(status().isOk()).andReturn();
+        tokenString = result.getResponse().getContentAsString();
+        map = mapper.readValue(tokenString, new TypeReference<Map<String, Object>>() {});
+        assertTrue(map.containsKey("user_name"));
+    }
+
+    @Test
+    public void testCheckMe() throws Exception {
+        // Invoke the token url and receive a token in return
+        MvcResult result = getResult();
+
+        String tokenString = result.getResponse().getContentAsString();
+        // Map the response
+        Map<String, Object> map = mapper.readValue(tokenString, new TypeReference<Map<String, Object>>() {});
+
+        result = this.mockMvc.perform(get("/user/me")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header("Authorization","Bearer "+map.get("access_token")))
+                .andExpect(status().isOk())
+                .andReturn();
+        tokenString = result.getResponse().getContentAsString();
+        map = mapper.readValue(tokenString,new TypeReference<Map<String,Object>>() {});
+        assertTrue(map.containsKey("username"));
+    }
+
+    @Test
+    public void testEncrypt() throws Exception {
+        // Invoke the token url and receive a token in return
+        MvcResult result = getResult();
+
+        String tokenString = result.getResponse().getContentAsString();
+        // Map the response
+        Map<String, Object> map = mapper.readValue(tokenString, new TypeReference<Map<String, Object>>() {});
+
+        result = this.mockMvc.perform(post("/encrypt")
+                .header("Authorization","Bearer "+map.get("access_token"))
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("test"))
+                .andExpect(status().isOk()).andReturn();
+        String encryptedPassword = result.getResponse().getContentAsString();
+        System.out.println(encryptedPassword);
+        assertNotNull(encryptedPassword);
+    }
+
+    // Helper method to Invoke the token url and receive a token in return
+    private MvcResult getResult() throws Exception {
+        return this.mockMvc.perform(post("/oauth/token")
+                .with(httpBasic(clientId,clientSecret))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .content("grant_type=password&username=tester&password=123456"))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 }
