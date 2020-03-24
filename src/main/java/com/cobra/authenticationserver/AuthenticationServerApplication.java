cef1a9ead7fbb657b8042ea145c8bb13d5a1c032
Octothorp6 <blora616@gmail.com>
2020-03-24 16:32:50 -0400

    props

--- src/main/java/com/cobra/authenticationserver/AuthenticationServerApplication.java
+++ src/main/java/com/cobra/authenticationserver/AuthenticationServerApplication.java
@@ -2,11 +2,13 @@ package com.cobra.authenticationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableResourceServer
 @EnableAuthorizationServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
